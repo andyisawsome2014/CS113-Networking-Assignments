d386515e19e0bdaee6cc8a6ce4daaa25d2cf6441
MalcolmChen97 <502829548@qq.com>
2017-10-25 21:14:18 -0700

    problem solved

--- hw2/code/client.py
+++ hw2/code/client.py
@@ -7,10 +7,14 @@ file_name=sys.argv[3]
 
 clientSocket = socket(AF_INET,SOCK_STREAM)
 clientSocket.connect((server_host,int(server_port)))
-httpRequest = "GET /"+file_name+" HTTP/1.1\r\nHost: "+server_host+"\r\nConnection: keep-alive\r\nUser-agent: Malcolm\r\nAccept-language: en"
+httpRequest = "GET /"+file_name+" HTTP/1.1\r\nHost: "+server_host+":"+server_port+"\r\nConnection: keep-alive\r\nUser-agent: Malcolm\r\nAccept-language: en"
 
 clientSocket.send(httpRequest.encode())
 response = clientSocket.recv(1024)
-print (response.decode())
+final=""
+while response:
+    final+=response.decode()
+    response = clientSocket.recv(1024)
+print (final)
 
 clientSocket.close()
--- hw2/code/server.py
+++ hw2/code/server.py
@@ -14,23 +14,23 @@ while True:
 
     print ("Ready to serve...")
     connectionSocket, addr = serverSocket.accept() #Fill in start Fill in end
-    print(addr)
+
+
 
     try:
 
         message = connectionSocket.recv(1024)
         filename = message.split()[1]
-        print(message)
         f = open(filename[1:], "rb")
         outputdata = f.read()#Fill in start Fill in end
         print("output is" + str(outputdata))
         #Send one HTTP header line into socket
             #Fill in start
         httpheader = "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nData: Today\r\nContent-Length:"+str(len(outputdata))+"\r\nContent-Type: text/html\r\n\r\n"
-        print(httpheader)
         connectionSocket.send(httpheader.encode())
             #Fill in end
         #Send the content of the requested file to the client
+        print(len(outputdata))
         for i in range(0,len(outputdata)):
             connectionSocket.send(outputdata[i:i+1])
         connectionSocket.send(b'\r\n\r\n')
